c295115931d72e09a23ee10fb49cbefa25ff7d9a
Andrew Melton <andrew.melton@rackspace.com>
2016-02-17 16:24:00 -0500

    Initials skeleton

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(name='userapi',
+      version='0.0.1',
+      description="userapi",
+      author='Andrew Melton',
+      author_email='ramielrowe@gmail.com',
+      packages=find_packages())
