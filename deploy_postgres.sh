fe00367f53a02c2e5abad59c7e5ff88a6a96e7d8
Andrew Melton <andrew.melton@rackspace.com>
2016-02-17 22:57:30 -0500

    Make deploy_postgres.sh idempotent

--- deploy_postgres.sh
+++ deploy_postgres.sh
@@ -2,13 +2,17 @@
 
 . env.sh
 
-docker create --name ${ENV_NAME}userapi_postgres_volumes \
-    postgres
+if [ -z "$(docker ps --filter name==${ENV_NAME}userapi_postgres_volumes)" ]; then
+    docker create --name ${ENV_NAME}userapi_postgres_volumes \
+        postgres
+fi
 
-docker run --name ${ENV_NAME}userapi_postgres \
-    -d --restart always \
-    --volumes-from ${ENV_NAME}userapi_postgres_volumes \
-    -e POSTGRES_USER=${POSTGRES_USER} \
-    -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
-    -e POSTGRES_DB=${POSTGRES_DB} \
-    postgres
+if [ -z "$(docker ps --filter name==${ENV_NAME}userapi_postgres)" ]; then
+    docker run --name ${ENV_NAME}userapi_postgres \
+        -d --restart always \
+        --volumes-from ${ENV_NAME}userapi_postgres_volumes \
+        -e POSTGRES_USER=${POSTGRES_USER} \
+        -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
+        -e POSTGRES_DB=${POSTGRES_DB} \
+        postgres
+fi
